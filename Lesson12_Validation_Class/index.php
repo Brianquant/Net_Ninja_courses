836d433425cad6b0f96ddc027ced0ad34e5664c9
Brianquant <reifferscheid.brianquant@gmail.com>
2021-09-14 12:49:05 +0200

    validateUser Class implemented

--- Lesson12_Validation_Class/index.php
+++ Lesson12_Validation_Class/index.php
@@ -1,3 +1,16 @@
+<?php 
+
+require('user_validator.php');
+    $errors = [];
+  if(isset($_POST['submit'])){
+    $validation = new UserValidator($_POST);
+    $errors = $validation->validateForm();
+  }
+
+?>
+
+
+
 <html lang="en">
 <head>
   <title>PHP OOP</title>
@@ -9,11 +22,15 @@
     <h2>Create a new user</h2>
     <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="POST">
       <label>username: </label>
-      <input type="text" name="username">
-
+      <input type="text" name="username" value="<?php echo htmlspecialchars($_POST['username'] ?? '') ?>">
+     <div class="error">
+        <?php echo $errors['username'] ?? ''?>
+     </div>
       <label>email: </label>
-      <input type="text" name="email">
-
+      <input type="text" name="email" value="<?php echo htmlspecialchars($_POST['email'] ?? '') ?>">
+      <div class="error">
+        <?php echo $errors['email'] ?? ''?>
+     </div>
       <input type="submit" value="submit" name="submit">
 
     </form>
@@ -21,14 +38,6 @@
 </body>
 </html>
 
-<?php 
-
-  if(isset($_POST['submit'])){
-    // validate entries
-    echo 'form submitted';
-  }
-
-?>
 
 
 
--- Lesson12_Validation_Class/user_validator.php
+++ Lesson12_Validation_Class/user_validator.php
@@ -1,86 +1,63 @@
 <?php 
 
-
-// create a user validator class to handle validation
-// constructor which takes in POST data from form
-// check required  "fields to check" are present in the data
-// create methods to validate individual fields
-// -- a method to validate a username
-// -- a method to validate an email
-
-
-// Create a UserValidator Class
 class UserValidator {
-// Properties Section
-// Assign a data prop private
-private $data;
-// Aissign a error prop to an empty array
-private $error = [];
-// Assign a static fields prop to the input fields inside an array
-private static $fields = ['username','email'];
 
+  private $data;
+  private $errors = [];
+  private static $fields = ['username', 'email'];
 
-// create construct function with post data (public)
-    public function __construct($postData) 
-    {
-        $this->data = $postData;
+  public function __construct($post_data){
+    $this->data = $post_data;
+  }
+
+  public function validateForm(){
+
+    foreach(self::$fields as $field){
+      if(!array_key_exists($field, $this->data)){
+        trigger_error("'$field' is not present in the data");
+        return;
+      }
     }
 
-// create validate Form function
-    private function formValidator() {
-        foreach (self::$fields as $field) { //the self keyword access the static property
-            if(!array_key_exists($field, $this->data)) {
-                trigger_error("$field is not present in data");
-                return;
-            }
-        }
+    $this->validateUsername();
+    $this->validateEmail();
+    return $this->errors;
 
-        $this->usernameValidator();
-        $this->emailValidator();
+  }
 
-        return $this->errors;
-    }
-// create validate Username function
-    private function usernameValidator() 
-    {
-        $val = trim($this->data[data['username']]);
+  private function validateUsername(){
 
-        if(empty($val)) {
-            $this->addError('username', 'username cannot be emtpy'); // The arguments are a key and value pair
-        } else {
-            if(!preg_match('/^[a-zA-Z0-9]{6,12}$/', $val)) { //preg_match takes 2 params. First (Regex) param checks for lowercase, uppercase, numbers and 6-12 Characters
-                $this->addError('username', 'username must be 6-12 characters and alpahnumeric!');
-            }
-        }
-    }
-// create validate Email function
-    private function emailValidator()
-    {
-        $val = trim($this->data[data['email']]);
+    $val = trim($this->data['username']);
 
-        if(empty($val)) {
-            $this->addError('email', 'Email cannot be emtpy'); // The arguments are a key and value pair
-        } else {
-            if(!filter_var($val, FILTER_VALIDATE_EMAIL)) { 
-                $this->addError('email', 'email must be a valid email');   
+    if(empty($val)){
+      $this->addError('username', 'username cannot be empty');
+    } else {
+      if(!preg_match("/^[a-zA-Z0-9]{6,12}$/", $val)){
+        $this->addError('username','username must be 6-12 chars & alphanumeric');
+      }
+    //   return;
     }
-// create validate addError function
-    private function addError($key, $val) 
-    {
-        $this->errors[$key] = $val;
+
+  }
+
+  private function validateEmail(){
+
+    $val = trim($this->data['email']);
+
+    if(empty($val)){
+      $this->addError('email', 'email cannot be empty');
+    } else {
+      if(!filter_var($val, FILTER_VALIDATE_EMAIL)){
+        $this->addError('email', 'email must be a valid email address');
+      }
     }
+
+  }
+
+  private function addError($key, $val){
+    $this->errors[$key] = $val;
+  }
+
 }
-
-
-
-
-
-
-
-
-
-
- 
-
 
 ?>
